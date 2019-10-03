8d01138285aefc365be79a73250c90df9bf95915
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2019-10-03 10:00:44 +0530

    added function prototype

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // contains all the controllers related to javascript
+
+//Budget Controller
+
+var budgetController= (function(){
+
+})();
+
+
+
+// UI Controller
+var UIController= (function(){
+
+
+})();
+
+
+// the global app controller
+
+var controller= (function(budgetctrl,uictrl){
+    
+
+})(budgetctrl,uictrl);
